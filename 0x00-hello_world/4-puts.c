b1a1bcc38a07c0f530240b723369288fcead9d98
Zelefack Marie Noelle <zelefackmarie858@gmail.com>
2023-08-10 11:54:18 -0700

    see

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+*main -this is the main function and it is where the program begins
+*Return: it is 0
+*/
+int main(void)
+{puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
