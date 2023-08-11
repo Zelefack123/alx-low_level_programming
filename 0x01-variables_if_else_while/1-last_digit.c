76f435b4bda3f85718c1a3151b637b9d23d5690f
Zelefack Marie Noelle <zelefackmarie858@gmail.com>
2023-08-11 05:40:57 -0700

    dee

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,9 +19,9 @@ m = n % 10;
 if (m > 5)
 printf("Last digit of %d is %d and is greater than 5", n, m);
 else if (m == 0)
-printf("Last digit of %d is %d and is zero", n, m);
+printf("Last digit of %d is %d and is equal to 0", n, m);
 else if (m < 6 && m != 0)
-printf("Last digit of %d is %d and is less than 6 and not equal to 6", n, m);
+printf("Last digit of %d is %d and is less than 6 and not equal to 0", n, m);
 printf("\n");
 return (0);
 }
