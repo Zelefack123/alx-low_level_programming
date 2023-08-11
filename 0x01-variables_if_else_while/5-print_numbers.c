1d159604df64987ff82f88a1f187b1885c3042e8
Zelefack Marie Noelle <zelefackmarie858@gmail.com>
2023-08-11 15:11:26 -0700

    see

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - is the first function
+ * Return: its 0
+*/
+int main(void)
+{
+int n;
+for (n = 0 ; n <= 9 ; n++)
+{
+printf("%d", n);
+}
+printf("\n");
+return (0);
+}
