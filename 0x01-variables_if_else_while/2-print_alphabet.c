5ca5f08c4e19af2a97004c3851918f7f158550ff
Zelefack Marie Noelle <zelefackmarie858@gmail.com>
2023-08-11 06:05:12 -0700

    see

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main - its the first function
+*
+*Return: its 0
+*/
+int main(void)
+{
+char i;
+for (i = 'a' ; i <= 'z' ; i++)
+putschar(i);
+putschar('\n');
+return (0);
+}
