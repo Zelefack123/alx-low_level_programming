94ad0bcda3c40036b14310f37b5528ce60d63932
Zelefack Marie Noelle <zelefackmarie858@gmail.com>
2023-08-11 17:50:16 -0700

    see

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - its easy
+ * Return: its 0
+ */
+int main(void)
+{
+int i;
+char c;
+for (i = 0; i <= 9; i++)
+putchar(i + '0');
+for (c = 'a'; c <= 'f'; c++)
+putchar(c);
+putchar('\n');
+return (0);
+}
