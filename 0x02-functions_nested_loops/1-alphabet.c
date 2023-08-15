561c09d50d50a4aa3d04f8a8bdf1b15e714b38bc
Zelefack Marie Noelle <zelefackmarie858@gmail.com>
2023-08-15 06:06:02 -0700

    see

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,21 +3,13 @@
  * main - thid
  * Return: thiss
  */
-void print_alphabet_x10(void);
 int main(void)
 {
-	void print_alphabet_x10(void);
-	return(0);
-}
-void print_alphabet_x10(void)
-{
 char i;
-int i;
-	for(i = 1 ; i <= 10; i++)
-	{
-	for (i = 'a' ; i <= 'z' ; i++)
-	{
-	putchar(i);
-	}
-	putchar('\n');
-	}
+for (i = 'a' ; i <= 'z' ; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return ('0');
+}
