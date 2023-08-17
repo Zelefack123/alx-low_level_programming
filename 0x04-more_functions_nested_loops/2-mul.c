376fd073ac86ff734204709a778fe6eec8675d2f
Zelefack Marie Noelle <zelefackmarie858@gmail.com>
2023-08-17 08:56:36 -0700

    see

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multiplies teo intergers
+ * @a: - the first interger
+ * @b: - the second interger
+ * Return: 0
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
