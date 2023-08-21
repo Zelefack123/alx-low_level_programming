ca6ba736e04618192e6b067f724c88c05aefe3cb
Zelefack Marie Noelle <zelefackmarie858@gmail.com>
2023-08-21 10:59:07 -0700

    SEE

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _putchar - writes the character to stdout
+ * @s:used to reference
+ *Return: 0
+ */
+void print_rev(char *s)
+{
+	int start = 0;
+
+	while (s[start])
+		start++;
+	while (start--)
+		_putchar(s[start]);
+	_putchar('\n');
+}
